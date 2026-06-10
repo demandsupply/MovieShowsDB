b71bfe379f9b55798dd07d9ae9d1b4bd7e16b9a4
demandsupply <giova.zol92@gmail.com>
2026-06-10 11:48:00 +0200

    refactor: use async function to fetch all seasons data in parallel, improving shows with many seasons loading time

--- routes/shows.py
+++ routes/shows.py
@@ -1,13 +1,14 @@
+import asyncio
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from helpers.postgreeDbQueries import *
 from helpers.utils import tmdb_get, format_runtime
 # from helpers.decorators import login_required
+from cs50 import SQL
 import ast
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 shows_bp = Blueprint("shows", __name__)
 
@@ -17,7 +18,7 @@ db = SQL(os.getenv("DATABASE_URL"))
 
 
 @shows_bp.route("/tvshow/<id>", methods = ["GET", "POST"])
-def tvshow_id(id):
+async def tvshow_id(id):
     print(f"id is {id}")
 
     user_id = session.get("user_id")
@@ -25,37 +26,35 @@ def tvshow_id(id):
 
 
     compare = []
-    seasons_episodes_average_vote = []
-    button_favorite_episodes = []
-
-    imgShow_datas = tmdb_get(f"tv/{id}/images?language=en")
     
-    if not imgShow_datas:
-        return None
+    # Parallelize initial TMDB calls
+    img_task = asyncio.to_thread(tmdb_get, f"tv/{id}/images?language=en")
+    details_task = asyncio.to_thread(tmdb_get, f"tv/{id}")
+    
+    results = await asyncio.gather(img_task, details_task)
+    imgShow_datas = results[0]
+    show_datas = results[1]
 
-    show_datas = tmdb_get(f"tv/{id}")
     print("SHOW DATAS", show_datas)
     if not show_datas:
         flash("TV show not found")
         return redirect("/")
 
     show_title = show_datas["name"]
-    # print(f"THIS IS SHOW DATAS {show_datas['name']}")
     number_of_seasons = show_datas["number_of_seasons"]
-    print(f"{show_datas["name"]} has {number_of_seasons} seasons")
 
     status = get_item_status(username, id, "tv-show")
     button_favorites = "remove from favorites" if status["is_favorite"] else "add to favorites"
     button_watchlist = "remove from watchlist" if status["is_in_watchlist"] else "add to watchlist"
 
-    
+
     # FUNCTION TO GET ALL EPISODES DATA
-    def load_all_episodes():
+    async def load_all_episodes():
         all_episodes = []
         favorite_buttons = []
         season_ratings = []
         counter = 0
-
+        
         # fetch all favorite episode IDs of the user and show in one query
         favorite_episode_ids = set()
         if user_id:
@@ -65,22 +64,22 @@ def tvshow_id(id):
             )
             favorite_episode_ids = {row["episode_id"] for row in fav_rows}
 
+        # Fetch all seasons in parallel
+        tasks = [asyncio.to_thread(tmdb_get, f"tv/{id}/season/{s}") for s in range(1, number_of_seasons + 1)]
+        seasons_results = await asyncio.gather(*tasks)
 
-        for season in range(1, number_of_seasons + 1):
-            season_data = tmdb_get(f"tv/{id}/season/{season}")
+        for season_data in seasons_results:
             if not season_data:
                 continue
-        
+            
             ratings = []
 
             for episode in season_data["episodes"]:
                 all_episodes.append(episode)
                 counter += 1
                 ratings.append(episode["vote_average"])
-
                 is_fav = episode["id"] in favorite_episode_ids
                 favorite_buttons.append("Unfavorite" if is_fav else "Add Favorite")
-
             season_ratings.append(ratings)
 
         return all_episodes, favorite_buttons, season_ratings, counter
@@ -158,65 +157,44 @@ def tvshow_id(id):
 
     #     return render_template ("tvshow.html", button_favorite_episodes=button_favorite_episodes, button_favorites=button_favorites, button_watchlist=button_watchlist, number_of_seasons=number_of_seasons, show_datas=show_datas, episodes_data=episodes_data, imgShow_datas=imgShow_datas)
 
-    if request.method == ("GET"):
-        eps, favorite_buttons, ratings, total = load_all_episodes()
-
+    if request.method == "GET":
+        eps, favorite_buttons, ratings, total = await load_all_episodes()
         session["ratings"] = ratings
         session["numberEpisodes"] = total
 
-        if user_id:
-            return render_template(
-                "tvshow.html",
-                show_datas=show_datas,
-                imgShow_datas=imgShow_datas,
-                number_of_seasons=number_of_seasons,
-                episodes_data=eps,
-                button_favorite_episodes=favorite_buttons,
-                button_favorites=button_favorites,
-                button_watchlist=button_watchlist
-            )
-        else:
-            return render_template(
+        return render_template(
             "tvshow.html",
             show_datas=show_datas,
             imgShow_datas=imgShow_datas,
             number_of_seasons=number_of_seasons,
-            episodes_data=eps
+            episodes_data=eps,
+            button_favorite_episodes=favorite_buttons,
+            button_favorites=button_favorites,
+            button_watchlist=button_watchlist
         )
     
     else:
         if user_id: 
-            compare = db.execute("SELECT show_title, username FROM compareshows WHERE show_title = %s AND username = %s", show_title, username) 
-            
             favorite_action = request.form.get("favorite")
-            if favorite_action:
-                if favorite_action == "add to favorites":
-                    add_favorite(username, id, "tv-show", show_datas["name"])
-                    button_favorites = "remove from favorites"
-                elif favorite_action == "remove from favorites":
-                    remove_favorite(username, id)
-                    button_favorites == "add to favorites"
+            
+            if favorite_action == "add to favorites":
+                add_favorite(username, id, "tv-show", show_datas["name"])
+            elif favorite_action == "remove from favorites":
+                remove_favorite(username, id)
 
             watchlist_action = request.form.get("watchlist")
-            if watchlist_action:
-                if watchlist_action == "add to watchlist":
-                    add_to_watchlist(username, id, "tv-show", show_datas["name"])
-                    button_watchlist == "remove from watchlist"
-                elif watchlist_action == "remove from watchlist":
-                    remove_from_watchlist(username, id)
-                    button_watchlist == "add to watchlist"
-                    
-
-
+            if watchlist_action == "add to watchlist":
+                add_to_watchlist(username, id, "tv-show", show_datas["name"])
+            elif watchlist_action == "remove from watchlist":
+                remove_from_watchlist(username, id)
                             
             if request.form.get('favorite_episodes'): 
-                episode_to_db_string = request.form.get('favorite_episodes') 
-                print(f"EPISODE STRING DATAS ARE: {episode_to_db_string}")
-                episode_to_db = ast.literal_eval(episode_to_db_string) 
-                print(f"IIIIIIIDDDDDD IS ONE {episode_to_db['id']}")
-
-                check_episode_on_db_list = db.execute("SELECT episode_id FROM usershows where episode_id = %s", episode_to_db["id"])
-
+                episode_data_str = request.form.get('favorite_episodes') 
+                print(f"EPISODE STRING DATAS ARE: {episode_data_str}")
+                episode_to_db = ast.literal_eval(episode_data_str)
+                print(f"IIIIIIIDDDDDD IS ONE {episode_to_db['id']}") 
+                
+                check_episode_on_db_list = db.execute("SELECT episode_id FROM usershows where episode_id = %s AND username = %s", episode_to_db["id"], username)
                 if check_episode_on_db_list:
                     print("episode saved on favorite episodes, I'll remove it")
                     remove_favorite_episode(username, episode_to_db["id"])
@@ -229,18 +207,20 @@ def tvshow_id(id):
                     )
             
             if request.form.get('compare'):
-                if not compare:
-                    list_to_string = ''.join(str(x) for x in seasons_episodes_average_vote)
+                # only 'ratings' values are needed
+                _, _, ratings, _ = await load_all_episodes()
+                list_to_string = str(ratings)
+                check_compare = db.execute("SELECT id FROM compareshows WHERE username = %s AND show_title = %s", username, show_title)
+                
+                if not check_compare:
                     db.execute("INSERT INTO compareshows (username, show_title, episodes_ratings) VALUES (%s, %s, %s)", username, show_title, list_to_string)
-                return redirect(url_for("ajaxshows"))
-            else:
-                print("no button clicked")
+                return redirect(url_for("compare.ajaxshows"))
 
             return redirect(url_for("shows.tvshow_id", id=id))
-
+        return redirect(url_for("auth.login"))
 
 @shows_bp.route("/tvshow/tv/<id>/season/<season>/episode/<seasonEpisode>", methods = ["GET", "POST"])
-def episode(id, season, seasonEpisode):
+async def episode(id, season, seasonEpisode):
     print(f"id {id}, season{season}, seasonEpisode{seasonEpisode}")
 
     user_id = session.get("user_id")
@@ -248,7 +228,7 @@ def episode(id, season, seasonEpisode):
 
 
     show_datas = tmdb_get(f"tv/{id}")   
-
+    
     if not show_datas:
         flash("TV show not found")
         return redirect("/")
@@ -256,8 +236,8 @@ def episode(id, season, seasonEpisode):
     show_title = show_datas["name"]
     # print(f"THIS IS SHOW DATAS {show_datas['name']}")
     number_of_seasons = show_datas["number_of_seasons"]
-    print(f"There are {number_of_seasons} seasons")     
-    
+    print(f"There are {number_of_seasons} seasons")  
+
     episode_data = tmdb_get(f"tv/{id}/season/{season}/episode/{seasonEpisode}")
 
     if not episode_data:
@@ -280,12 +260,8 @@ def episode(id, season, seasonEpisode):
 
         if action == "remove from favorite episodes":
             remove_favorite_episode(username, episode_id)
-
+            
         elif action == "add to favorite episodes":
-            add_favorite_episode(username[0]["username"], show_title, id, episode_data["season_number"], episode_data["episode_number"], episode_data["name"], episode_data["id"])
+            add_favorite_episode(username, show_title, id, episode_data["season_number"], episode_data["episode_number"], episode_data["name"], episode_data["id"])
 
-        else:
-            print("no button clicked")
-
-        return redirect(url_for("episode", id=id, season=season, seasonEpisode=seasonEpisode))
-
+        return redirect(url_for("shows.episode", id=id, season=season, seasonEpisode=seasonEpisode))
